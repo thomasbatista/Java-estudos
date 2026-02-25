93871b29b7594afeac57932ac35c6ed934a2e49c
Thomas Batista <139007458+thomasbatista@users.noreply.github.com>
2026-02-24 21:56:05 -0300

    começando POO

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.domain.Estudante;
+
+public class EstudanteTest01 {
+    static void main() {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
