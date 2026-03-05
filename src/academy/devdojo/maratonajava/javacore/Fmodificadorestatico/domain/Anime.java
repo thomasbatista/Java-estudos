67af4cb914fde3e815c662878116f90c0bd8b3bf
Thomas Batista <139007458+thomasbatista@users.noreply.github.com>
2026-03-04 22:03:52 -0300

    static

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Anime.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+    static {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for (int episodio : episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Anime;
+
+public class AnimeTest01 {
+    static void main() {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+
+
+    }
+}
