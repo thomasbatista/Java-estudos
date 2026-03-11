2e0e1f89d8ed7028d45d0a12907e65a3389809fd
Thomas Batista <139007458+thomasbatista@users.noreply.github.com>
2026-03-10 22:08:49 -0300

    enumerações

---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Kenum.test;
+
+import academy.devdojo.maratonajava.javacore.Kenum.domain.Cliente;
+import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoPagamento;
+
+public class ClienteTest01 {
+    static void main() {
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        TipoCliente tipoCliente2 = TipoCliente.tipoCLientePorNomeRelatorio("Pessoa Física");
+        System.out.println(tipoCliente.getNomeRelatorio());
+        System.out.println(tipoCliente2);
+    }
+}
